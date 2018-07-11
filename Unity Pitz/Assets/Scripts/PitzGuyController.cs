7c875dad42b8b4f00e4534423199fd73b50256fc
Unknown <gabrielbeal@gmail.com>
2018-07-11 00:24:29 -0400

    Got the basics working
    You can fall in a pit and lose. You can go to the goal and win. Controls are WASD.

---
+++ Unity Pitz/Assets/Scripts/PitzGuyController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitzGuyController : MonoBehaviour
+{
+	
+	public float pitzGuySpeed;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		if (Input.GetKey (KeyCode.A))
+		{
+			Vector3 newDirection = new Vector3 (-pitzGuySpeed * Time.deltaTime, 0, 0);
+			transform.position += newDirection;
+		}
+		
+		if (Input.GetKey (KeyCode.D))
+		{
+			Vector3 newDirection = new Vector3 (pitzGuySpeed * Time.deltaTime, 0, 0);
+			transform.position += newDirection;
+		}
+		
+		if (Input.GetKey (KeyCode.W))
+		{
+			Vector3 newDirection = new Vector3 (0, pitzGuySpeed * Time.deltaTime, 0);
+			transform.position += newDirection;
+		}
+		
+		if (Input.GetKey (KeyCode.S))
+		{
+			Vector3 newDirection = new Vector3 (0, -pitzGuySpeed * Time.deltaTime, 0);
+			transform.position += newDirection;
+		}
+	}
+}
