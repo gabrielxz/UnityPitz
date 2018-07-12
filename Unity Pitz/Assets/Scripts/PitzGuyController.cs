52046905b5ccd43f5bb3fa661c07e598b1bfdf6b
Unknown <gabrielbeal@gmail.com>
2018-07-11 20:39:09 -0400

    Changed the player control
    Now using a momentum-type system. Controls more like a ship/boat, but it feels better to me. Also changed camera background to black.

--- Unity Pitz/Assets/Scripts/PitzGuyController.cs
+++ Unity Pitz/Assets/Scripts/PitzGuyController.cs
@@ -4,41 +4,31 @@ using UnityEngine;
 
 public class PitzGuyController : MonoBehaviour
 {
-	
-	public float pitzGuySpeed;
-	
-	// Use this for initialization
-	void Start ()
-	{
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		
-		if (Input.GetKey (KeyCode.A))
-		{
-			Vector3 newDirection = new Vector3 (-pitzGuySpeed * Time.deltaTime, 0, 0);
-			transform.position += newDirection;
-		}
-		
-		if (Input.GetKey (KeyCode.D))
-		{
-			Vector3 newDirection = new Vector3 (pitzGuySpeed * Time.deltaTime, 0, 0);
-			transform.position += newDirection;
-		}
-		
-		if (Input.GetKey (KeyCode.W))
-		{
-			Vector3 newDirection = new Vector3 (0, pitzGuySpeed * Time.deltaTime, 0);
-			transform.position += newDirection;
-		}
-		
-		if (Input.GetKey (KeyCode.S))
-		{
-			Vector3 newDirection = new Vector3 (0, -pitzGuySpeed * Time.deltaTime, 0);
-			transform.position += newDirection;
-		}
-	}
+
+    public float speed;             //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
+    }
 }
