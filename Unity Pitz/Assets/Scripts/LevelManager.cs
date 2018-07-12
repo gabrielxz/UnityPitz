31595d7a48ca588fc443589554d1354e58331c8b
Unknown <gabrielbeal@gmail.com>
2018-07-11 20:44:00 -0400

    Removed deprecated method
    I was using a deprecated method to change scenes.

--- Unity Pitz/Assets/Scripts/LevelManager.cs
+++ Unity Pitz/Assets/Scripts/LevelManager.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
 
-	public void LoadLevel(string name){
-		Debug.Log ("New Level load: " + name);
-		Application.LoadLevel (name);
-	}
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
 
-	public void QuitRequest(){
-		Debug.Log ("Quit requested");
-		Application.Quit ();
-	}
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
 
 }
