9424e346693546e90e463cd11fb44e7b75632093
Unknown <gabrielbeal@gmail.com>
2018-07-15 10:57:06 -0600

    Added a Shinigami to hunt the player
    Right now, he doesn't move. But if the player touches him, he dead.

---
+++ Unity Pitz/Assets/Scripts/shinigami.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shinigami : MonoBehaviour {
+
+	private LevelManager levelManager;
+	
+	void Start ()
+	{
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+	}
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		levelManager.LoadLevel ("Lose Screen");
+	}
+}
