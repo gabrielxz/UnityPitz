cbb058605c75fcb5f306ffb000c419d3a828acb8
Richard Harris <rbharris42@gmail.com>
2018-09-04 21:30:49 -0400

    Basic Pointer
    Adds a basic pointer to the objective

---
+++ Unity Pitz/Assets/Scripts/Compass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+public class Compass : MonoBehaviour
+{
+    public Quaternion MissionDirection;
+    public Transform Arrow;
+    public Transform Missionplace;
+
+    void Start()
+    {
+        Arrow = GameObject.FindGameObjectWithTag("Arrow").GetComponent<Transform>();
+        Missionplace = GameObject.FindGameObjectWithTag("Goal").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        ChangeMissionDirection();
+    }
+    void ChangeMissionDirection()
+    {
+        Vector3 dir = Arrow.position - Missionplace.position;
+        MissionDirection = Quaternion.LookRotation(dir);
+        MissionDirection.z = -MissionDirection.x;
+        MissionDirection.x = 0;
+        MissionDirection.y = 0;
+        Arrow.rotation = MissionDirection;
+        transform.Rotate(0, 0, 90);
+    }
+}
